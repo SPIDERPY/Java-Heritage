f8f9059106234542f5fb40ef3c3218229a222a07
AB <ab@AB.lan>
2024-01-18 13:45:16 +0100

    Java 07: Héritage

---
+++ Hangar.java
@@ -0,0 +1,10 @@
+public class Hangar {
+
+    public static void main(String[] args) {
+        Car clio = new Car("Clio");
+        System.out.println(clio.doStuff());
+
+        Boat titanic = new Boat("Titanic");
+        System.out.println(titanic.doStuff());
+    }   
+}
